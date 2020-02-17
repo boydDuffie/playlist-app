67251d76edbc29d844620fa7f3aab4407fb048b3
boydDuffie <shmeeves@ufl.edu>
2020-02-17 16:26:33 -0500

    added some functions to playlist.js. There is a better way to implement the structure (see TODOs)

--- playlist.js
+++ playlist.js
@@ -4,8 +4,12 @@
 //songs will be an array of song objects
 //color will be an object with 3 properties: R, G, and B
 
+//TODO: implement inheritance to make a parent playlist class
+	//colorPlaylists should inherit all general playlist properties but should have a color field
+
 const colorPlaylistFactory = (id, owner, songs, color) => {
 	return {
+		//want to protect these properties
 		_id: id,
 		_owner: owner, 
 		_songs: songs, 
@@ -31,6 +35,32 @@ const colorPlaylistFactory = (id, owner, songs, color) => {
 			if(typeof newColor === "object" && newColor.R && newColor.G && newColor.B)
 			{
 				this._color = newColor;
+			}
+		},
+
+	//FUNCTIONALITY
+		addSong(newSong) {
+			if(newSong.id)	//newSong needs an id
+			{
+				let duplicate = false;
+				//check that newSong isn't already in playlist
+				//each song in playlist is checked by id against newSong; if no matches are found then song is pushed to this._songs array
+				this._songs.forEach(item => {
+					if(item.id === newSong.id) 
+					{
+						console.log(`Cannot add song; ${newSong.name} is already in this playlist.`);
+						duplicate = true;
+					}
+				});
+				if(!duplicate)
+				{
+					this._songs.push(newSong);
+					return;
+				}
+				else
+				{
+					return;
+				}
 			}
 		}
 	}

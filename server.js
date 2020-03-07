74d0c2acfc42c9a620494069cd0b43ce541150e4
boydDuffie <shmeeves@ufl.edu>
2020-03-07 18:41:16 -0500

    added spotify API info (client id / secret) to server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//Spotify API variables
+const client_id = '1df847690a434522b6194652b2dc79bb';
+const client_secret = '9cd2b264279545b19be4eefe92a92fa7';
+const redirect_uri = 'http://localhost:' + PORT + '/callback';
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -15,6 +20,7 @@ app.use(cors());
 
 app.use(errorHandler());
 
+//start the server:
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
